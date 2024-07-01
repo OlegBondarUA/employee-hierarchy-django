1befe7c83bd8295755dde7f48170417aacf418af
OlegBondarUA <Petrovichteh@gmail.com>
2024-07-01 16:22:58 +0300

    Added lazy loading for employee hierarchy using AJAX

--- employees/urls.py
+++ employees/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employee_hierarchy/', views.employee_hierarchy, name='employee_hierarchy'),
+    path('', views.employee_hierarchy, name='employee_hierarchy'),
+    path('load_subordinates/<int:employee_id>/', views.load_subordinates, name='load_subordinates'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
 from .models import Employee
 
 
 def employee_hierarchy(request):
-    root_employees = Employee.objects.filter(manager__isnull=True)
+    root_employees = Employee.objects.filter(manager__isnull=True)[:10]
+    return render(request, 'employees/employee_hierarchy.html', {'root_employees': root_employees})
 
-    context = {
-        'root_employees': root_employees,
+
+def load_subordinates(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    subordinates = employee.subordinates.all()[:10]
+    data = {
+        'html': ''.join(
+            f'<li data-employee-id="{subordinate.id}">{subordinate.full_name} ({subordinate.position})'
+            f'<ul class="subordinates"></ul>'
+            f'<button class="load-more" data-employee-id="{subordinate.id}">Load more...</button>'
+            f'</li>'
+            for subordinate in subordinates
+        )
     }
-    return render(request, 'employees/employee_hierarchy.html', context)
+    return JsonResponse(data)
